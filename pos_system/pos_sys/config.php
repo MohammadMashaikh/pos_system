62d09fe1831db0050d193d57d9c9b131019ea988
MohammadMashaikh <90157227+MohammadMashaikh@users.noreply.github.com>
2023-02-01 00:26:49 +0300

    Create config.php

---
+++ pos_system/pos_sys/config.php
@@ -0,0 +1,7 @@
+<?php 
+// Change this config depend on your database to connect it .
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'system');
